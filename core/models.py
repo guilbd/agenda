1d5aec313d88c6348c1ee2e4b1a79443ff8cc353
guilbd <57806160+guilbd@users.noreply.github.com>
2020-07-11 20:13:24 -0300

    Add files via upload

---
+++ core/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+
+# Create your models here.
+from django.db.models import ForeignKey
+
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    local = models.TextField(blank=True, null = True)
+    descricao = models.TextField(blank = True, null= True, verbose_name='Descrição do Evento')
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hs')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
